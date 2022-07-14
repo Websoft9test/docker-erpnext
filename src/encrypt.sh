a273e2dea541d0bc828f9691d450a7043feb8102
qiaofeng1227 <76487013@qq.com>
2022-07-14 10:40:03 +0800

    Create encrypt.sh

---
+++ src/encrypt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
+clear
+
+
+
+# Add encrypt script below if use APP_ENCRYPT_PASSWORD at .env file
+# ------------- start -------------------------
+
+
+
+# ------------- end ---------------------------
