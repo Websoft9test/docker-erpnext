8e60f209a25d4a081fd722f44c1aba1c6ac3b55b
qiaofeng1227 <76487013@qq.com>
2022-07-16 16:07:24 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "erpnext version:" $(cat /data/apps/erpnext/.env |grep  "APP_VERSION" |awk -F"=" '{print $2}') |sudo tee -a /data/logs/install_version.txt
+sudo echo "erpnext version:" $(cat /data/apps/erpnext/.env |grep  "APP_VERSION" |awk -F"=" '{print $2}') 1>> /data/logs/install_version.txt
